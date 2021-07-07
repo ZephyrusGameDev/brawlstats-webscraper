be463708693c4a51d8d99ef9151a083975747ac7
ZephyrusGameDev <65769983+ZephyrusGameDev@users.noreply.github.com>
2021-07-07 08:17:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestingWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter player tag:");
+            string tag = Console.ReadLine();
+
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://brawlstats.com/profile/" + tag);
+
+            var trophies = doc.DocumentNode.SelectSingleNode("//div[@class='mo25VS9slOfRz6jng3WTf']").InnerText.ToString();
+
+            int count = int.Parse(trophies);
+        }
+    }
+}
